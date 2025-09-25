711ec7b17ce1fcbc525fab6a10a2f606f73e59e1
KikolasDEV <kikoamarillo94@gmail.com>
2025-09-26 00:25:49 +0200

    Clases internas, anónimas y lambdas

--- bases/src/com/ipartek/formacion/pruebas/ImprimiblePrueba.java
+++ bases/src/com/ipartek/formacion/pruebas/ImprimiblePrueba.java
@@ -1,10 +1,13 @@
 package com.ipartek.formacion.pruebas;
 
 import com.ipartek.formacion.pojos.Impresora;
+import com.ipartek.formacion.pojos.Imprimible;
 import com.ipartek.formacion.pojos.Local;
 import com.ipartek.formacion.pojos.Persona;
 
 public class ImprimiblePrueba {
+	private static final int NUMERO_CARACTERES = 50;
+	
 	public static void main(String[] args) {
 		Persona persona = new Persona("Javier");
 		
@@ -12,9 +15,38 @@ public class ImprimiblePrueba {
 		
 		Impresora impresora = new Impresora();
 		
+		impresora.agregarImprimibile(new Separador(NUMERO_CARACTERES));
 		impresora.agregarImprimibile(local);
+		impresora.agregarImprimibile(new SeparadorClaseInterna());
 		impresora.agregarImprimibile(persona);
-		
+		impresora.agregarImprimibile(new Imprimible() {
+			@Override
+			public String imprimir() {
+				return "+".repeat(NUMERO_CARACTERES);
+			}
+		});
+		impresora.agregarImprimibile(() -> "Hecho por Javier");
+		impresora.agregarImprimibile(() -> "*".repeat(NUMERO_CARACTERES));
 		impresora.imprimir();
+	}
+	
+	static class SeparadorClaseInterna implements Imprimible {
+		@Override
+		public String imprimir() {
+			return "=".repeat(NUMERO_CARACTERES);
+		}
+	}
+}
+
+class Separador implements Imprimible {
+	private Integer numeroCaracteres;
+	
+	public Separador(Integer numeroCaracteres) {
+		this.numeroCaracteres = numeroCaracteres;
+	}
+	
+	@Override
+	public String imprimir() {
+		return "-".repeat(numeroCaracteres);
 	}
 }
