4cdb469b087c323659277bda750e47a0915ead28
KikolasDEV <kikoamarillo94@gmail.com>
2025-09-30 13:59:44 +0200

    Corrección del DAO

--- bases/src/com/ipartek/bibliotecas/Dao.java
+++ bases/src/com/ipartek/bibliotecas/Dao.java
@@ -2,11 +2,9 @@ package com.ipartek.bibliotecas;
 
 import java.util.Optional;
 
-import com.ipartek.citas.entidades.Cita;
-
 public interface Dao<T> {
 	Iterable<T> obtenerTodos();
-	Optional<Cita> obtenerPorId(Long id);
+	Optional<T> obtenerPorId(Long id);
 	
 	T insertar(T cita);
 	T modificar(T cita);
