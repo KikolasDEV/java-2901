a0f98b52f19cd0d1162705a6091deeae7b6a2828
KikolasDEV <kikoamarillo94@gmail.com>
2025-10-02 09:46:04 +0200

    Creación a nivel global de la URL, USER y PASS de la conexión

---
+++ bases/src/com/ipartek/citas/accesodatos/Globales.java
@@ -0,0 +1,8 @@
+package com.ipartek.citas.accesodatos;
+
+public class Globales {
+	static final String URL = "jdbc:sqlite:bdd/citas.db?foreign_keys=on";
+	static final String USER = "";
+	static final String PASS = "";
+	
+}
