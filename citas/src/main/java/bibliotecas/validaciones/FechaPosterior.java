6cc26da38b4247861e332f314aa047c1767e74de
KikolasDEV <kikoamarillo94@gmail.com>
2025-10-14 13:56:03 +0200

    Jakarta Validation con Hibernate Validator como implementación

---
+++ citas/src/main/java/bibliotecas/validaciones/FechaPosterior.java
@@ -0,0 +1,23 @@
+package bibliotecas.validaciones;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Documented
+@Constraint(validatedBy = FechaPosteriorValidator.class)
+@Target({ ElementType.TYPE }) // Se aplica a nivel de clase
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FechaPosterior {
+    String message() default "La fecha final debe ser posterior a la fecha inicial";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+    String fechaInicio();
+    String fechaFin();
+}
