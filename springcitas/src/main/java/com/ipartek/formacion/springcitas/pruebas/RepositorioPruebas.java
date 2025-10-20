9a3af8c269371d2050640fba5f88172e384a3a7c
KikolasDEV <kikoamarillo94@gmail.com>
2025-10-20 11:21:01 +0200

    Repositorios

---
+++ springcitas/src/main/java/com/ipartek/formacion/springcitas/pruebas/RepositorioPruebas.java
@@ -0,0 +1,39 @@
+package com.ipartek.formacion.springcitas.pruebas;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.ipartek.formacion.springcitas.entidades.Rol;
+import com.ipartek.formacion.springcitas.entidades.Usuario;
+import com.ipartek.formacion.springcitas.repositorios.RolRepository;
+import com.ipartek.formacion.springcitas.repositorios.UsuarioRepository;
+
+@Component
+public class RepositorioPruebas implements CommandLineRunner {
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private RolRepository rolRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		var admin = Rol.builder().nombre("ADMIN").build();
+		var user = Rol.builder().nombre("USER").build();
+		
+		rolRepository.save(admin);
+		rolRepository.save(user);
+		
+		usuarioRepository.save(Usuario.builder().nombre("Kiko").email("kiko@email.net").password("kiko").rol(admin).build());
+		usuarioRepository.save(Usuario.builder().nombre("Javier").email("javier@email.net").password("javier").rol(user).build());
+		usuarioRepository.save(Usuario.builder().nombre("Juan").email("juan@email.net").password("juan").rol(user).build());
+		
+		usuarioRepository.findAll().forEach(System.out::println);
+		
+		System.out.println();
+		
+		usuarioRepository.findByEmail("javier@email.net").ifPresentOrElse(System.out::println, () -> System.out.println("No se ha encontrado"));
+	}
+
+}
