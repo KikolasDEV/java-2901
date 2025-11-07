11d486d4630d704a035acf1dbb07350de5a22ab6
KikolasDEV <kikoamarillo94@gmail.com>
2025-11-07 12:34:48 +0100

    Carrito en sesión con agregar producto

--- amazonia/src/main/java/com/ipartek/formacion/amazonia/controladores/IndexController.java
+++ amazonia/src/main/java/com/ipartek/formacion/amazonia/controladores/IndexController.java
@@ -6,23 +6,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.ipartek.formacion.amazonia.servicios.AnonimoService;
+import com.ipartek.formacion.amazonia.sesion.Carrito;
 
 @Controller
 public class IndexController {
 	@Autowired
 	private AnonimoService anonimoService;
-	
+
 	@GetMapping
 	public String index(Model modelo, Long idCategoria) {
 		modelo.addAttribute("productos", anonimoService.listadoProductos(idCategoria));
-		
+
 		return "index";
 	}
-	
+
 	@GetMapping("detalle")
 	public String detalle(Long id, Model modelo) {
 		modelo.addAttribute("producto", anonimoService.detalleProducto(id));
 
 		return "detalle";
+	}
+	
+	@GetMapping("carrito")
+	public String carrito(Long id, Carrito carrito) {
+		carrito.agregarProducto(anonimoService.detalleProducto(id));
+		
+		return "redirect:/";
 	}
 }
